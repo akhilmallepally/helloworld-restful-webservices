3aed2c04fb89516dc960b14d6fae9bb1cc444bfd
akhilmallepally <akhilnani831@gmail.com>
2022-03-19 19:41:51 -0500

    update: added return value in createuser method

--- src/main/java/com/restapidemo/ui/controller/UserController.java
+++ src/main/java/com/restapidemo/ui/controller/UserController.java
@@ -1,15 +1,19 @@
 package com.restapidemo.ui.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.restapidemo.ui.model.request.UserDetailsRequestModel;
 import com.restapidemo.ui.model.response.UserRest;
 
 @RestController
@@ -24,19 +28,26 @@ public class UserController {
 	}
 
 	@GetMapping(path="/{userId}", produces= {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
-	public UserRest getUser(@PathVariable String userId) {
+	public ResponseEntity<UserRest> getUser(@PathVariable String userId) {
 		UserRest returnValue = new UserRest();
 		returnValue.setEmail("mallepallyakhil@gmail.com");
 		returnValue.setFirstName("Akhil");
 		returnValue.setLastName("Mallepally");
 		
-		return returnValue;
-		
+		return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);	
 	}
 	
-	@PostMapping
-	public String createUser() {
-		return "create user";
+	@PostMapping(consumes= {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+			produces= {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+	
+	public ResponseEntity<UserDetailsRequestModel> createUser(@RequestBody UserDetailsRequestModel userDetails) {
+		UserDetailsRequestModel returnValue = new UserDetailsRequestModel();
+		returnValue.setEmail(userDetails.getEmail());
+		returnValue.setFirstName(userDetails.getFirstName());
+		returnValue.setLastName(userDetails.getLastName());
+		returnValue.setPassword(userDetails.getPassword());
+		
+		return new ResponseEntity<UserDetailsRequestModel>(returnValue, HttpStatus.OK);
 	}
 	
 	@PutMapping
